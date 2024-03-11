28ebb766959986424fef8e146df3f45fb7b1fbeb
19BCE0590-Niranjan <lakshminiranjanp@gmail.com>
2024-03-11 18:52:06 +0530

    initial commit

---
+++ sample_proj/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Header from './components/Header'
+import Food from './components/Food'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
