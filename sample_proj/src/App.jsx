655a5c18dbcd5076e56c39aee49a88f5d4f1673d
19BCE0590-Niranjan <lakshminiranjanp@gmail.com>
2024-03-12 11:21:02 +0530

    initial commit

--- sample_proj/src/App.jsx
+++ sample_proj/src/App.jsx
@@ -1,14 +1,33 @@
 
-import UserGreeting from './components/UserGreeting'
+import List from './components/List'
 
 
 function App() {
 
-  return (
-    <>
-      <UserGreeting isLoggedIn = {false} username="Niranjan"/>
-    </>
-  )
+
+      const fruits = [{id: 1 ,name:"apple", calories : 95},
+                    {id: 2 ,name:"orange" , calories :45},
+                    {id: 3 ,name:"banana", calories : 105},
+                    {id: 4 ,name:"coconut" , calories :156},
+                    {id: 5 ,name: "pineapple", calories :37}];
+
+
+
+      const vegetables = [{id: 6 ,name:"potatoes", calories : 110},
+                          {id: 7 ,name:"celebry" , calories :15},
+                          {id: 8 ,name:"carrots", calories : 25},
+                          {id: 9 ,name:"corn" , calories :63},
+                          {id: 10 ,name: "brocolli", calories :50}];
+
+
+
+
+  return (<>
+  {fruits.length > 0 && <List items={fruits} category="Fruits"/> }
+  {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/> }
+  
+   
+  </>)
 }
 
 export default App
